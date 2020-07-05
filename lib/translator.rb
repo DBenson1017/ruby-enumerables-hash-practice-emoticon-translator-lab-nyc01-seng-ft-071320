9c4a6cbf353ba7bed0187e1abe17f4df4d4224a3
Devin Benson <devinbenson@gmail.com>
2020-07-05 16:13:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,6 +20,7 @@ def get_japanese_emoticon(file_path, e_emo)
   dictionary.each do |key, value|
    if e_emo == value[:english] 
      return value[:japanese] 
+     binding.pry 
    end 
    "Sorry, that emoticon was not found"
  end 
