14620e3bfd81569bf0c4ff34e8fd27c90d4539c7
Devin Benson <devinbenson@gmail.com>
2020-07-05 16:21:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,7 +26,7 @@ def get_japanese_emoticon(file_path, e_emo)
 end 
 
 
-def get_english_meaning(j_emo)
+def get_english_meaning(file_path, j_emo)
    dictionary = load_library("./lib/emoticons.yml")
     dictionary.each do |key, value|
       if key[:japanese] == j_emo 
