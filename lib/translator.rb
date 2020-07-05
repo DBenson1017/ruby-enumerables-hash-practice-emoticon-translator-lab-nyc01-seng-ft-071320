148996cab52b1dd3360a3e39c93d9380ca4dabb1
Devin Benson <devinbenson@gmail.com>
2020-07-05 16:06:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,8 @@ end
 def get_japanese_emoticon(file_path, e_emo)
   dictionary = load_library("./lib/emoticons.yml")
   dictionary.each do |key, value|
-   if e_emo == value[:english] return value[:japanese] 
+   if e_emo == value[:english] 
+     return value[:japanese] 
    end 
    "Sorry, that emoticon was not found"
 
